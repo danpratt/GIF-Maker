51a5663daac88c02c465222376197fbae360480a
Daniel Pratt <daniel@blaumagier.com>
2017-05-04 11:27:26 -0700

    feat: added blank view controller files
    all files in this update are shells, and contain no code
    
    - added VC's to handle:
    	* welcome screen
    	* editor screen
    	* preview screen
    
    - added extension to UIViewController to handle recording video

---
+++ GifMaker_Swift_Template/Controllers/Extensions/UIViewController+Record.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewController+Record.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Daniel Pratt on 5/4/17.
+//  Copyright © 2017 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MobileCoreServices
+
+extension UIViewController {
+    
+    
+    
+}
