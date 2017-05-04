94d8f2e780e9e7e688693e3c6bd90e4a6f485903
Daniel Pratt <daniel@blaumagier.com>
2017-05-04 11:53:45 -0700

    feat: added the ability to record video using device camera
    - setup camera record in UIViewController extension
    - connected extension to camera button on Storyboard
    - unable to test on device yet, but crashing in simulator (expected behavor since simulator doesn't have camera)

--- GifMaker_Swift_Template/Controllers/Extensions/UIViewController+Record.swift
+++ GifMaker_Swift_Template/Controllers/Extensions/UIViewController+Record.swift
@@ -10,8 +10,39 @@ import Foundation
 import UIKit
 import MobileCoreServices
 
-extension UIViewController {
+extension UIViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    // MARK: - launch video camera
+    
+    @IBAction func launchVideoCamera(sender: AnyObject) {
+        let recordVideoController = UIImagePickerController()
+        recordVideoController.sourceType = .camera
+        recordVideoController.mediaTypes = [kUTTypeMovie as String]
+        // will change to true later
+        recordVideoController.allowsEditing = false
+        recordVideoController.delegate = self
+        present(recordVideoController, animated: true, completion:  nil)
+    }
+    
+    
+    // MARK: - imagePickerController delegate methods
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let mediaType = info[UIImagePickerControllerMediaType] as! String
+        
+        if mediaType == kUTTypeMovie as String {
+            
+            let videoURL = info[UIImagePickerControllerMediaURL] as! URL
+            UISaveVideoAtPathToSavedPhotosAlbum(videoURL.path, nil, nil, nil)
+            dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
     
     
 }
